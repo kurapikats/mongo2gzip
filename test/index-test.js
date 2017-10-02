098c5da43ba540d42f7051c8308fbcc74b92446c
kurapikats <kurapikats@yahoo.com>
2017-10-02 19:29:56 +0800

    add canary test

---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect
+const mongo2gz = require('../index')
+
+describe('mongo2gz-test', () => {
+  it('should pass this canary test', () => {
+    expect(true).to.be.true
+  })
+})
